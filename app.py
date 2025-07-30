96a2a6657cadfaf8fcdfa57b53f3a92f2ca350ee
prestontabor21 <preston.tabor21@gmail.com>
2025-07-30 14:49:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -25,17 +25,40 @@ if uploaded_files:
     st.write(avg_df)
 
     # Example filters (you’ll expand later)
-    pitch_type = st.selectbox("Select Pitch Type", avg_df[pitch_type_col].unique())
-    min_spin = st.slider("Minimum Spin Rate (RPM)", 0, 3500, 2000)
-    min_hbreak = st.slider("Minimum Horizontal Break (in)", -30, 30, 10)
-    min_vbreak = st.slider("Minimum Vertical Break (in)", -60, 60, 10)
+#    pitch_type = st.selectbox("Select Pitch Type", avg_df[pitch_type_col].unique())
+#    min_spin = st.slider("Minimum Spin Rate (RPM)", 0, 3500, 2000)
+#    min_hbreak = st.slider("Minimum Horizontal Break (in)", -30, 30, 10)
+#    min_vbreak = st.slider("Minimum Vertical Break (in)", -60, 60, 10)
 
-    filtered = avg_df[
-        (avg_df[pitch_type_col] == pitch_type) &
-        (avg_df['SpinRate'] >= min_spin) &
-        (avg_df['HorzBreak'] >= min_hbreak) &
-        (avg_df['VertBreak'] >= min_vbreak)
-    ]
+ #   filtered = avg_df[
+ #       (avg_df[pitch_type_col] == pitch_type) &
+ #       (avg_df['SpinRate'] >= min_spin) &
+ #       (avg_df['HorzBreak'] >= min_hbreak) &
+ #       (avg_df['VertBreak'] >= min_vbreak)
+ #   ]
+
+    pitch_type = st.selectbox("Select Pitch Type", avg_df[pitch_type_col].unique())
+
+    use_spin = st.checkbox("Filter by Spin Rate", value=True)
+    if use_spin:
+        min_spin = st.slider("Minimum Spin Rate (RPM)", 0, 3500, 2000)
+    use_hbreak = st.checkbox("Filter by Horizontal Break", value=True)
+    if use_hbreak:
+        min_hbreak = st.slider("Minimum Horizontal Break (in)", -30, 30, 10)
+    use_vbreak = st.checkbox("Filter by Vertical Break", value=True)
+    if use_vbreak:
+        min_vbreak = st.slider("Minimum Vertical Break (in)", -60, 60, 10)
+
+    # Build filter mask dynamically
+    mask = (avg_df[pitch_type_col] == pitch_type)
+    if use_spin:
+        mask &= (avg_df['SpinRate'] >= min_spin)
+    if use_hbreak:
+        mask &= (avg_df['HorzBreak'] >= min_hbreak)
+    if use_vbreak:
+        mask &= (avg_df['VertBreak'] >= min_vbreak)
+
+    filtered = avg_df[mask]
 
     display_cols = [player_id_col, pitch_type_col, "RelSpeed", "SpinRate", "SpinAxis", "Extension", "VertBreak", "HorzBreak"]
     filtered_display = filtered[display_cols]
